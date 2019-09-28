195f7e7c4b3becd1a8041782e88dd3c1c3df74b8
xuzhiqin1990 <xuzhiqin1990@qq.com>
2019-09-28 17:13:14 +0400

    Update main_1d_20190924.py

--- main_1d_20190924.py
+++ main_1d_20190924.py
@@ -2,6 +2,13 @@
 #email: xuzhiqin@sjtu.edu
 #2019-09-24
 # coding: utf-8
+'''
+Reference: 
+1 Training behavior of deep neural network in frequency domain: https://arxiv.org/abs/1807.01251
+2 Frequency Principle: Fourier Analysis Sheds Light on Deep Neural Networks: https://arxiv.org/abs/1901.06523
+3 Explicitizing an Implicit Bias of the Frequency Principle in Two-layer Neural Networks: https://arxiv.org/abs/1905.10264
+4 Theory of the Frequency Principle for General Deep Neural Networks: https://arxiv.org/abs/1906.09235
+'''
 
 
 #import sys
